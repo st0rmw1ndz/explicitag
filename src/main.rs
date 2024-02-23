5165ab09bb1e258f08a07d45ff44caa063a28efd
frosty <inthishouseofcards@gmail.com>
2024-02-23 07:10:57 -0500

    fix structural issues - using a logger crate - made some matches into `if`s with early return - task creation is cleaner - `no_write` option - using more `.map`

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@ use std::sync::atomic::{AtomicBool, AtomicUsize, Ordering};
 use std::sync::Arc;
 
 use clap::{ColorChoice, Parser};
+use env_logger::Env;
 use eyre::eyre;
+use log::LevelFilter;
 use mp4ameta::{AdvisoryRating, Tag};
 use reqwest::header::{self, HeaderMap, HeaderValue};
 use reqwest::Client;
@@ -16,10 +18,20 @@ const CLIENT_USER_AGENT: &str = "explicitag, https://github.com/st0rm1wndz/expli
 static MARK_CLEAN: AtomicBool = AtomicBool::new(false);
 static USE_API: AtomicBool = AtomicBool::new(false);
 static WRITE_LYRICS: AtomicBool = AtomicBool::new(false);
-static QUIET: AtomicBool = AtomicBool::new(false);
+static NO_WRITE: AtomicBool = AtomicBool::new(false);
 
 static PROCESSED_FILES: AtomicUsize = AtomicUsize::new(0);
 static SKIPPED_FILES: AtomicUsize = AtomicUsize::new(0);
+
+#[derive(Debug, Deserialize)]
+struct LyristResponse {
+    lyrics: Option<String>,
+}
+
+enum LyricsSource {
+    Api,
+    Local,
+}
 
 #[derive(Debug, Parser)]
 #[command(about, long_about = None, after_help = env!("CARGO_PKG_REPOSITORY"), version, color = ColorChoice::Never)]
@@ -36,23 +48,22 @@ struct Cli {
     #[arg(short = 'a', long, default_value_t = false)]
     use_api: bool,
 
-    /// Write the lyrics from the API to the file
+    /// Write lyrics from the API to files
     #[arg(short = 'w', long, default_value_t = true, requires = "use_api")]
     write_lyrics: bool,
+
+    /// Disable writing lyrics and ratings to files
+    #[arg(
+        short = 'n',
+        long,
+        default_value_t = false,
+        conflicts_with = "write_lyrics"
+    )]
+    no_write: bool,
 
     /// Suppress all output from the program
     #[arg(short = 'q', long, default_value_t = false)]
     quiet: bool,
-}
-
-#[derive(Debug, Deserialize)]
-struct LyristResponse {
-    lyrics: Option<String>,
-}
-
-enum LyricsSource {
-    Local,
-    API,
 }
 
 async fn search_lyrist(artist: &str, title: &str, client: &Client) -> eyre::Result<String> {
@@ -67,10 +78,10 @@ async fn search_lyrist(artist: &str, title: &str, client: &Client) -> eyre::Resu
     let text = response.text().await?;
     let lyrist_response: LyristResponse = serde_json::from_str(&text)?;
 
-    match lyrist_response.lyrics {
-        Some(lyrics) => Ok(lyrics.to_lowercase()),
-        None => Err(eyre!("No track was found via API")),
-    }
+    lyrist_response
+        .lyrics
+        .map(|lyrics| lyrics.to_lowercase())
+        .ok_or_else(|| eyre!("No track was found via API"))
 }
 
 async fn get_track_lyrics(tag: &Tag, client: &Client) -> eyre::Result<(String, LyricsSource)> {
@@ -78,18 +89,18 @@ async fn get_track_lyrics(tag: &Tag, client: &Client) -> eyre::Result<(String, L
         return Ok((lyrics.to_lowercase(), LyricsSource::Local));
     }
 
-    if USE_API.load(Ordering::Relaxed) {
-        if let (Some(artist), Some(title)) = (tag.artist(), tag.title()) {
-            match search_lyrist(artist, title, client).await {
-                Ok(lyrics) => return Ok((lyrics, LyricsSource::API)),
-                Err(error) => return Err(error),
-            }
-        } else {
-            return Err(eyre!("Artist and title are required"));
-        }
+    if !USE_API.load(Ordering::Relaxed) {
+        return Err(eyre!("No lyrics found"));
     }
 
-    Err(eyre!("No lyrics found"))
+    let artist = tag
+        .artist()
+        .ok_or_else(|| eyre!("Artist tag is required"))?;
+    let title = tag.title().ok_or_else(|| eyre!("Title tag is required"))?;
+
+    search_lyrist(artist, title, client)
+        .await
+        .map(|lyrics| (lyrics, LyricsSource::Api))
 }
 
 fn get_lyrics_rating(lyrics: String, explicit_words: &[&str]) -> AdvisoryRating {
@@ -105,104 +116,102 @@ fn get_lyrics_rating(lyrics: String, explicit_words: &[&str]) -> AdvisoryRating 
     }
 }
 
-async fn process_file(path: &Path, explicit_words: &[&str], client: &Client) -> eyre::Result<()> {
-    let mut tag = Tag::read_from_path(path)?;
+async fn process_file(
+    path: &Path,
+    explicit_words: &[&str],
+    client: &Client,
+) -> eyre::Result<String> {
+    let mut tag = Tag::read_from_path(path).map_err(|_| eyre!("Invalid MP4 file"))?;
 
-    match get_track_lyrics(&tag, client).await {
-        Ok((lyrics, lyrics_source)) => {
-            if let LyricsSource::API = lyrics_source {
-                if WRITE_LYRICS.load(Ordering::Relaxed) {
-                    tag.set_lyrics(&lyrics);
-                }
-            }
-
-            let new_rating = get_lyrics_rating(lyrics, explicit_words);
-
-            if tag.advisory_rating() != Some(new_rating) {
-                tag.set_advisory_rating(new_rating);
-                tag.write_to_path(path)?;
-
-                if !QUIET.load(Ordering::Relaxed) {
-                    let source = match lyrics_source {
-                        LyricsSource::API => "API",
-                        LyricsSource::Local => "Local",
-                    };
-                    println!("{} - ({}) {}", path.display(), source, new_rating);
-                }
-
-                return Ok(());
-            }
-
-            if !QUIET.load(Ordering::Relaxed) {
-                println!("{} - Rating is the same as current", path.display());
-            }
-            SKIPPED_FILES.fetch_add(1, Ordering::Relaxed);
-        }
-        Err(error) => {
-            if !QUIET.load(Ordering::Relaxed) {
-                println!("{} - {}", path.display(), error);
-            }
-            SKIPPED_FILES.fetch_add(1, Ordering::Relaxed);
-        }
+    let (lyrics, lyrics_source) = get_track_lyrics(&tag, client).await?;
+    let source_string = match lyrics_source {
+        LyricsSource::Api => "API",
+        LyricsSource::Local => "Local",
     };
 
-    Ok(())
+    if let LyricsSource::Api = lyrics_source {
+        if WRITE_LYRICS.load(Ordering::Relaxed) && !NO_WRITE.load(Ordering::Relaxed) {
+            tag.set_lyrics(&lyrics);
+        }
+    }
+
+    let new_rating = get_lyrics_rating(lyrics, explicit_words);
+
+    if !NO_WRITE.load(Ordering::Relaxed) {
+        tag.set_advisory_rating(new_rating);
+        tag.write_to_path(path)?;
+    }
+
+    Ok(format!("({}) {}", source_string, new_rating))
+}
+
+async fn create_task(path: PathBuf, explicit_words: Arc<Vec<&str>>, client: Arc<Client>) {
+    PROCESSED_FILES.fetch_add(1, Ordering::Relaxed);
+    match process_file(&path, &explicit_words, &client).await {
+        Ok(message) => {
+            log::info!("{} - {}", path.display(), message);
+        }
+        Err(error) => {
+            SKIPPED_FILES.fetch_add(1, Ordering::Relaxed);
+            log::warn!("{} - {}", path.display(), error);
+        }
+    }
 }
 
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let args = Cli::parse();
 
     MARK_CLEAN.store(args.mark_clean, Ordering::Relaxed);
     USE_API.store(args.use_api, Ordering::Relaxed);
     WRITE_LYRICS.store(args.write_lyrics, Ordering::Relaxed);
-    QUIET.store(args.quiet, Ordering::Relaxed);
+    NO_WRITE.store(args.write_lyrics, Ordering::Relaxed);
 
-    let explicit_words: Arc<Vec<&str>> = Arc::new(include_str!("words.txt").split("\n").collect());
+    if args.quiet {
+        log::set_max_level(LevelFilter::Off);
+    } else {
+        log::set_max_level(LevelFilter::Info);
+    }
 
     let mut headers = HeaderMap::new();
     headers.insert(
         header::USER_AGENT,
         HeaderValue::from_str(CLIENT_USER_AGENT)?,
     );
-    let client: Arc<Client> = Arc::new(Client::builder().default_headers(headers).build()?);
 
-    if !QUIET.load(Ordering::Relaxed) {
-        println!(
-            "Running with {} words marked as explicit.",
-            explicit_words.len()
-        );
-    }
+    let explicit_words = Arc::new(include_str!("words.txt").split('\n').collect());
+    let client = Arc::new(Client::builder().default_headers(headers).build()?);
 
-    let mut tasks = vec![];
-
-    for path in args.paths {
-        for entry in WalkDir::new(path)
+    let entries = args.paths.into_iter().flat_map(|path| {
+        WalkDir::new(path)
             .into_iter()
             .filter_map(Result::ok)
             .filter(|e| e.file_type().is_file())
-        {
+    });
+    let tasks: Vec<_> = entries
+        .map(|entry| {
             let path = entry.path().to_path_buf();
             let explicit_words = Arc::clone(&explicit_words);
             let client = Arc::clone(&client);
-            let task = tokio::spawn(async move {
-                PROCESSED_FILES.fetch_add(1, Ordering::Relaxed);
-                process_file(&path, &explicit_words, &client).await
-            });
-            tasks.push(task);
-        }
-    }
 
-    let results = futures::future::try_join_all(tasks).await;
-    results?;
+            tokio::spawn(create_task(path, explicit_words, client))
+        })
+        .collect();
 
-    if !QUIET.load(Ordering::Relaxed) {
-        println!(
-            "Processed {} files. Skipped {} files.",
-            PROCESSED_FILES.load(Ordering::Relaxed),
-            SKIPPED_FILES.load(Ordering::Relaxed)
-        );
-    }
+    log::info!(
+        "Running with {} words marked as explicit.",
+        explicit_words.len()
+    );
+
+    futures::future::join_all(tasks).await;
+
+    log::info!(
+        "Processed {} files. Skipped {} files.",
+        PROCESSED_FILES.load(Ordering::Relaxed),
+        SKIPPED_FILES.load(Ordering::Relaxed)
+    );
 
     Ok(())
 }
